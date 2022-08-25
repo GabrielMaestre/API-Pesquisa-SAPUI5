7c80e59a9e913c0c3eaf8af24d6b18bc7f97b6e7
Gabriel Moreira <gmaestre002@gmail.com>
2022-08-24 21:40:07 -0300

    Commit Init
    Commit inicial

---
+++ List.controller.js
@@ -0,0 +1,52 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, Filter, FilterOperator, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.m.sample.ListSelectionSearch.List", {
+
+		onInit: function () {
+			
+			// Essa versão é antiga para usar .toUrl();
+			// https://stackoverflow.com/questions/56697573/uncaught-typeerror-sap-ui-require-tourl-is-not-a-function
+			var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/items.json"));
+			this.getView().setModel(oModel);
+		},
+
+		onSearch: function (oEvent) {
+			// add filter for search
+			var aFilters = [];
+			var sQuery = oEvent.getSource().getValue();
+			if (sQuery && sQuery.length > 0) {
+				var filter = new Filter("title", FilterOperator.Contains, sQuery);
+				aFilters.push(filter);
+			}
+
+			// update list binding
+			var oList = this.byId("idList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilters, "Application");
+		},
+
+		onSelectionChange: function (oEvent) {
+			var oList = oEvent.getSource();
+			var oLabel = this.byId("idFilterLabel");
+			var oInfoToolbar = this.byId("idInfoToolbar");
+
+			// With the 'getSelectedContexts' function you can access the context paths
+			// of all list items that have been selected, regardless of any current
+			// filter on the aggregation binding.
+			var aContexts = oList.getSelectedContexts(true);
+
+			// update UI
+			var bSelected = (aContexts && aContexts.length > 0);
+			var sText = (bSelected) ? aContexts.length + " selected" : null;
+			oInfoToolbar.setVisible(bSelected);
+			oLabel.setText(sText);
+		}
+
+	});
+});
